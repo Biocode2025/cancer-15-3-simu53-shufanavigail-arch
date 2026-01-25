46faa7843aa4435a52f9c8354de23146608f6915
shufanavigail-arch <shufan.avigail@gmail.com>
2026-01-25 20:38:10 +0200

    added a fuction that translates rna sequence into protein sequence

--- scripts/main.py
+++ scripts/main.py
@@ -15,3 +15,11 @@ def Read_dict(file): #פונקציה שקוראת את הקובץ של הקוד�
     (Codon, sep, Amino_Acid) = line.partition('\t') #הרדה של הקודון מהחומצת אמינו בקובץ
     #print(Amino_Acid)
     RNA_codon_table[Codon] = Amino_Acid
+
+def RNA_prot(seq): #פוקציה שמקבלת רצף רנא ומתרגמת אותו לרצף חלבונים
+  prot_seq = ''
+  for i in range(0, len(seq), 3): #לולאה שרצה על הרצף בקפיצות של 3
+    codon = seq[i : i+3] #כל קודון שווה לשלישייה מהרצף
+    Amino = RNA_codon_table[codon] #מציאת החומצת אמינו באמצעות המילון
+    prot_seq += Amino
+  return prot_seq
